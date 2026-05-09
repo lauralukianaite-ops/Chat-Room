ac9e44f820e167123257a3b1d8a896f4a606a44f
Laura <laura.lukianaite@gmail.com>
2026-05-09 20:29:36 +0300

    Making GUI

--- src/main/java/org/example/chatroom/ChatController.java
+++ src/main/java/org/example/chatroom/ChatController.java
@@ -2,13 +2,14 @@ package org.example.chatroom;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class ChatController {
-    @FXML
-    private Label welcomeText;
+    @FXML private Label usernameLabel;
+    @FXML private ListView<String> roomsList;
+    @FXML private ListView<String> usersList;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void setUsername(String vardas) {
+        usernameLabel.setText(vardas);
     }
 }
